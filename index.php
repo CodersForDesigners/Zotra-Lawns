58a79e2e1989e56d87caa3ee5e03af698663a001
adi <adityabhat@lazaro.in>
2018-04-12 17:22:26 +0530

    RERA Disclaimer; Add it

--- index.php
+++ index.php
@@ -832,12 +832,12 @@
 		<div class="container">
 			<div class="row">
 				<div class="columns small-10 small-offset-1">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<div class="heading h3">Disclaimer</div>
+					<div class="point small">By using or accessing the Website you agree with the Disclaimer without any qualification or limitation. Zotra reserves the right to terminate, revoke, modify, alter, add and delete any one or more of the terms and conditions of the website. Zotra shall be under no obligation to notify the visitor of the amendment to the terms and conditions and the visitor shall be bound by such amended terms and conditions.</div>
+					<div class="point small">The visuals and information contained herein marked as "artistic impression" are artistic impressions being indicative in nature and are for general information purposes only. The visuals contained marked as "generic image" and other visuals /image /photographs are general images and do not have any correlation with the project.</div>
+					<div class="point small">The imagery used on the website may not represent actuals or may be indicative of style only. Photographs of interiors, surrounding views and location may not represent actuals or may have been digitally enhanced or altered. These photographs may not represent actuals or may be indicative only. Computer generated images, walkthroughs and render images are the artist's impression and are an indicative of the actual designs.</div>
+					<div class="point small">No information given on this Website creates a warranty or expand the scope of any warranty that cannot be disclaimed under the applicable laws. The information on this website is presented as general information and no representation or warranty is expressly or impliedly given as to its accuracy. Any interested party should verify all the information including designs, plans, specifications, facilities, features, payment schedules, terms of sales etc independently with Zotra prior to concluding any decision for buying in any of the project.</div>
+					<div class="point small">While enough care is taken by Zotra to ensure that information in the website are up to date, accurate and correct, the readers/ users are requested to make an independent enquiry with Zotra before relying upon the same. Nothing on the website should be misconstrued as advertising, marketing, booking, selling or an offer for sale or invitation to purchase a unit in any project by Zotra. Zotra is not responsible for the consequences of any action taken by the viewer relying on such material/ information on this website without independently verifying with Zotra.</div>
 				</div>
 			</div>
 		</div>
