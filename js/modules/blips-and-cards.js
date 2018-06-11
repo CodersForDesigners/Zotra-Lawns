d626609eceeb33bb2a1f03440702ee01f6320389
adi <adityabhat@lazaro.in>
2018-06-11 10:48:22 +0530

    Blips and cards; Co-locate the cards with the blips for larger screens

--- js/modules/blips-and-cards.js
+++ js/modules/blips-and-cards.js
@@ -3,6 +3,22 @@ $( function () {
 
 	/*
 	 *
+	 * Copy the "point" cards over to where blip toggles are.
+	 * This redundancy is so that for larger screens, we can show the cards
+	 * right next to the blip toggles itself.
+	 *
+	 */
+	$( ".js_blip_canvas" ).each( function ( _i, el ) {
+		var $cardCarousel = $( el ).closest( ".js_section" ).find( ".js_slick_blip" );
+		var $points = $cardCarousel.find( ".js_point" );
+		var $pointsRedundant = $( el ).find( ".js_point" );
+		$pointsRedundant.each( function ( index ) {
+			$pointsRedundant.eq( index ).html( $points.eq( index ).html() );
+		} );
+	} );
+
+	/*
+	 *
 	 * Initialise the card carousels
 	 *
 	 */
@@ -32,19 +48,34 @@ $( function () {
 
 	/*
 	 *
-	 * On hitting a blip, "slide" to the corresponding card
+	 * On hitting a blip,
+	 * 1. Show the corresponding card, and anchor it next to the blip ( for larger screens ).
+	 * 2. "Slide" to the corresponding card in the card carousel ( for smaller screens ).
 	 *
 	 */
 	$( document ).on( "focus", ".js_blip_toggle", function ( event ) {
 
+		// Get the blip toggle element
 		var $blipToggle = $( event.target ).closest( ".js_blip_toggle" );
 
+		// Un-mark the card that is currently "active"
+		$blipToggle.closest( ".js_section" ).find( ".js_point" ).removeClass( "active" );
+
+		// Mark that blip toggle element as "active"
 		$blipToggle
 			.closest( ".js_section" )
 			.find( ".js_blip_toggle" )
 			.removeClass( "active" )
 		$blipToggle.addClass( "active" );
 
+		/*
+		 * Show the corresponding card, anchored next to the blip
+		 */
+		$blipToggle.parent().find( ".js_point" ).addClass( "active" );
+
+		/*
+		 * "Sliding" to the next card in the carousel
+		 */
 		// Get the index of the slide to "slide" to
 		var index = $blipToggle.data( "index" ) - 1;
 		// Get the corresponding slick widget for that section
